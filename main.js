05b43a8b34f3203653fab22ddbf248ffa625033c
Your Name <yan.vi193@gmail.com>
2022-01-10 15:48:29 +0100

    fix-img-in-js

--- main.js
+++ main.js
@@ -22,32 +22,32 @@ const team = [                                                  // Array di ogge
   {
     name: 'Wayne Barnett',
     role: 'Founder & CEO',
-    image: 'img/wayne-barnett-founder-ceo.jpg',
+    image: 'wayne-barnett-founder-ceo.jpg',
   },
   {
     name: 'Angela Caroll',
     role: 'Chief Editor',
-    image: 'img/angela-caroll-chief-editor.jpg',
+    image: 'angela-caroll-chief-editor.jpg',
   },
   {
     name: 'Walter Gordon',
     role: 'Office Manager',
-    image: 'img/walter-gordon-office-manager.jpg',
+    image: 'walter-gordon-office-manager.jpg',
   },
   {
     name: 'Angela Lopez',
     role: 'Social Media Manager',
-    image: 'img/angela-lopez-social-media-manager.jpg',
+    image: 'angela-lopez-social-media-manager.jpg',
   },
   {
     name: 'Scott Estrada',
     role: 'Developer',
-    image: 'img/scott-estrada-developer.jpg',
+    image: 'scott-estrada-developer.jpg',
   },
   {
     name: 'Barbara Ramos',
     role: 'Graphic Designer',
-    image: 'img/barbara-ramos-graphic-designer.jpg',
+    image: 'barbara-ramos-graphic-designer.jpg',
   },
 ];
 
@@ -61,7 +61,7 @@ for (let i = 0; i < team.length; i++){                          // Ciclo for per
   infoBox += `
               <div class="team-card">
                 <div class="card-image">
-                  <img src="${team[i].image}"/>
+                  <img src="img/${team[i].image}"/>
                 </div>
 
                 <div class="card-text">
