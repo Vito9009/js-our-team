d285aff160af106ddfe3fe80b6ff9ea4103e9f68
Your Name <yan.vi193@gmail.com>
2022-01-10 15:46:51 +0100

    Inserimento-dei-6-profili-esistenti

--- main.js
+++ main.js
@@ -18,35 +18,72 @@ Quando un pezzo di codice funziona, chiediamoci se possiamo scomporlo in funzion
  */
 
 
-const team = [
+const team = [                                                  // Array di oggetti
   {
     name: 'Wayne Barnett',
     role: 'Founder & CEO',
-    image: 'wayne-barnett-founder-ceo.jpg',
+    image: 'img/wayne-barnett-founder-ceo.jpg',
   },
   {
     name: 'Angela Caroll',
     role: 'Chief Editor',
-    image: 'angela-caroll-chief-editor.jpg',
+    image: 'img/angela-caroll-chief-editor.jpg',
   },
   {
     name: 'Walter Gordon',
     role: 'Office Manager',
-    image: 'walter-gordon-office-manager.jpg',
+    image: 'img/walter-gordon-office-manager.jpg',
   },
   {
     name: 'Angela Lopez',
     role: 'Social Media Manager',
-    image: 'angela-lopez-social-media-manager.jpg',
+    image: 'img/angela-lopez-social-media-manager.jpg',
   },
   {
     name: 'Scott Estrada',
     role: 'Developer',
-    image: 'scott-estrada-developer.jpg',
+    image: 'img/scott-estrada-developer.jpg',
   },
   {
     name: 'Barbara Ramos',
     role: 'Graphic Designer',
-    image: 'barbara-ramos-graphic-designer.jpg',
+    image: 'img/barbara-ramos-graphic-designer.jpg',
   },
 ];
+
+// Inserimento delle info contenute nell'array di oggetti all'interno del file HTML tramite il ciclo for e innerHTML
+
+let containerTeam = document.querySelector(".team-container");    // Selezionare il container in HTML dove stampare le info
+
+let infoBox = "";
+
+for (let i = 0; i < team.length; i++){                          // Ciclo for per creazione div da stampare in HTML
+  infoBox += `
+              <div class="team-card">
+                <div class="card-image">
+                  <img src="${team[i].image}"/>
+                </div>
+
+                <div class="card-text">
+                  <h3>${team[i].name}</h3>
+                  <p>${team[i].role}</p>
+                </div>    
+              </div>
+            `
+}
+
+containerTeam.innerHTML += infoBox;                             // Stampa in HTML le info dei 6 profili esistenti
+/*
+<div class="team-card">
+  <div class="card-image">
+    <img
+      src="img/wayne-barnett-founder-ceo.jpg"
+      alt="Wayne Barnett"
+    />
+  </div>
+  <div class="card-text">
+    <h3>Wayne Barnett</h3>
+    <p>Founder & CEO</p>
+  </div>
+</div>
+*/
